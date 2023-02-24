b1ff50e81df84c51315edeb6642ec206a3306f7c
mrunmaihingane-coditas <124657592+mrunmaihingane-coditas@users.noreply.github.com>
2023-02-24 16:44:33 +0530

    Add files via upload

---
+++ Q1NoofCharater.java
@@ -0,0 +1,13 @@
+import java.util.StringTokenizer;
+
+
+public class Q1NoofCharater {
+    public static void main(String[] args) {
+          TokenDemo tokenDemo = new TokenDemo();
+        System.out.println("Numbers of token :: " +tokenDemo.display());
+
+        CountVowels string = new CountVowels();
+        string.countVowels("Mrunmai");
+
+    }
+}
